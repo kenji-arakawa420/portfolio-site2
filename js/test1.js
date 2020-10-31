e604448a1fe67d74e767a4905e3633205cee0ca1
kenji-arakawa420 <73603780+kenji-arakawa420@users.noreply.github.com>
2020-11-01 01:57:20 +0900

    Add files via upload

---
+++ js/test1.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+})
---
+++ js/test2.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+$(function() {
+    var topBtn = $('.to-top');
+    //ボタンを非表示にする
+    topBtn.hide();
+    //スクロールしてページトップから100に達したらボタンを表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            //フェードインで表示
+            topBtn.fadeIn();
+        } else {
+            //フェードアウトで非表示
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップへ戻る
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
